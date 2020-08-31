5b2d8c67dc9ca2617142c9420c6dfbe533828856
9deok <qqqq3115@naver.com>
2020-08-31 23:11:51 +0900

    add Template for future algorithm

---
+++ gudeok/src/temPlate.java
@@ -0,0 +1,15 @@
+/**
+ * 문제 링크 : https://programmers.co.kr/learn/courses/30/lessons/12925
+ * 문제 설명 : 템플릿 사용 용도의 첫번째 문제 풀이. 폴더구조는 계속 생각중...
+ * 소요시간 : 3분
+ */
+
+public class temPlate {
+    public static void main(String[] args) {
+        System.out.println(solution("-1234"));
+    }
+
+    public static int solution(String s) {
+        return Integer.parseInt(s);
+    }
+}
