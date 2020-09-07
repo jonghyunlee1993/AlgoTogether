297f330599b77f39e0f73eb7da5e0cf5cc329e1c
9deok <qqqq3115@naver.com>
2020-09-07 09:51:55 +0900

    Second stretching

---
+++ gudeok/src/solution/solution2.java
@@ -0,0 +1,22 @@
+package solution;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+/**
+ * 문제 링크 : https://programmers.co.kr/learn/courses/30/lessons/42748
+ *
+ */
+public class solution2 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 24;
+        System.out.println(solution(a,b));
+    }
+
+    public static String solution(int a, int b) {
+        LocalDate localDate = LocalDate.of(2016,a,b);
+        String answer = localDate.getDayOfWeek().toString().substring(1,3);
+        return answer;
+    }
+}
