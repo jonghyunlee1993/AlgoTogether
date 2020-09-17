aa71f9b57134c2399e409bc620bfb8a552355725
9deok <qqqq3115@naver.com>
2020-09-17 10:00:36 +0900

    JosePhus

---
+++ gudeok/src/solution/Josephus.java
@@ -0,0 +1,39 @@
+package solution;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Josephus {
+    public static void main(String[] args) {
+        solution();
+    }
+
+    public static void solution() {
+        Scanner sc = new Scanner(System.in);
+        int peopleCount = sc.nextInt();
+        int orderNumber = sc.nextInt();
+        StringBuilder sb = new StringBuilder();
+        sb.append("<");
+
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i = 1; i <= peopleCount; i++) {
+            queue.offer(i);
+        }
+
+        for(int i =0; i < peopleCount-1; i++) {
+//            System.out.println("*************************");
+//            System.out.println(queue);
+//            System.out.println("*************************");
+            for(int j = 0; j < orderNumber-1; j++) {
+                queue.offer(queue.poll());
+            }
+//            System.out.println("=========================");
+//            System.out.println(queue);
+//            System.out.println("=========================");
+            sb.append(queue.poll() + ", ");
+        }
+        sb.append(queue.poll() + ">");
+        System.out.println(sb);
+    }
+}
