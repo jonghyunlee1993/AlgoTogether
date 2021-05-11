344773ea49e0d4dab5605967fcbccaac3089a1a7
9deok <qqqq3115@naver.com>
2021-05-11 15:34:25 +0900

    해시맵을 이용한 완주하지 못한 선수 복습

--- gudeok/src/hash/NotCompletePlayer.java
+++ gudeok/src/hash/NotCompletePlayer.java
@@ -4,26 +4,24 @@ import java.util.HashMap;
 
 public class NotCompletePlayer {
     public static void main(String[] args) {
-        String[] participant = {"marina", "josipa", "nikola", "vinko", "filipa"};
-        String[] completion = {"josipa", "filipa", "marina", "nikola"};
-        solution(participant , completion);
+        String[] participant = {"mislav", "stanko", "mislav", "ana"};
+        String[] completion = {"stanko", "ana", "mislav"};
+        System.out.println(solution(participant , completion));
     }
 
     public static String solution(String[] participant, String[] completion) {
         String answer = "";
-        HashMap<String, Integer> hm = new HashMap<String, Integer>();
+        HashMap<String, Integer> hm = new HashMap<>();
+        for (String player : participant)  hm.put(player, hm.getOrDefault(player, 0) + 1);
+        for (String player : completion)  hm.put(player, hm.get(player) - 1);
 
-        for (String player : participant)
-            hm.put(player, hm.getOrDefault(player, 0) + 1);
-
-        for (String player : completion)
-            hm.put(player, hm.get(player) - 1);
-
-        for (String key : hm.keySet()) {
-            if (hm.get(key) != 0)
+        for(String key : hm.keySet()) {
+            if(hm.get(key) != 0) {
                 answer = key;
+                break;
+            }
+
         }
-        System.out.println(answer);
         return answer;
     }
 }
