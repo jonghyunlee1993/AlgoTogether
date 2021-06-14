5e528f2edf2c82d6d5cd2b5396ae655530465ea8
9deok <butterflynine20@gmail.com>
2021-06-14 20:45:04 +0900

    프로그래머스 기능개발

---
+++ gudeok/src/baekjoon/Template2.java
@@ -0,0 +1,14 @@
+package baekjoon;
+
+/***
+ * 문제번호 :
+ * 문제 타이틀 :
+ * 정답비율 :
+ * 문제 링크 :
+ * 참고 :
+ */
+public class Template2 {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
