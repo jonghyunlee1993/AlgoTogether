e24ce43e5462caee0988cf4f530d419fae2e668f
9deok <butterflynine20@gmail.com>
2021-06-27 18:10:47 +0900

    프로그래머스 더 맵게

---
+++ gudeok/src/programmers/high_score/Heap1.java
@@ -0,0 +1,41 @@
+package programmers.high_score;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+/***
+ * 문제 타이틀: 더 맵게
+ * 문제 링크 : https://programmers.co.kr/learn/courses/30/lessons/42626
+ */
+public class Heap1 {
+    static int answer = 0;
+    public static void main(String[] args) {
+        int[] scoville = {1, 2, 3, 9, 10, 12};
+        int K = 7;
+        solution(scoville,K);
+    }
+
+    public static void solution(int[] scoville, int K) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for (int i = 0; i < scoville.length; i++) {
+            pq.add(scoville[i]);
+        }
+
+        int answer = 0;
+        while (pq.size() > 1 && pq.peek() < K) {
+            int weakHot = pq.poll();
+            int secondWeakHot = pq.poll();
+
+            int mixHot = weakHot + (secondWeakHot * 2);
+            pq.add(mixHot);
+            answer++;
+        }
+
+        if(pq.size() <= 1 && pq.peek() < K)
+            answer = -1;
+
+        System.out.println(answer);
+    }
+
+}
