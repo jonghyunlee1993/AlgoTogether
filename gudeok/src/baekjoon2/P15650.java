0c47357222c1bc62f2749da9434e098e25d09c0e
9deok <butterflynine20@gmail.com>
2021-09-01 19:53:48 +0900

    백준 15650 - 2

---
+++ gudeok/src/baekjoon2/P15650.java
@@ -0,0 +1,41 @@
+package baekjoon2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class P15650 {
+
+    private static int[] numbers;
+    private static StringBuilder answer = new StringBuilder();
+    private static int N;
+    private static int M;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        numbers = new int[M];
+
+        dfs(1,0);
+        System.out.println(answer);
+    }
+
+    private static void dfs(int start, int depth) {
+        if(depth == M) {
+            for(int number : numbers) {
+                answer.append(number).append(" ");
+            }
+            answer.append("\n");
+            return;
+        }
+
+        for (int i = start; i <= N; i++) {
+            numbers[depth] = i;
+            dfs(i + 1, depth + 1);
+        }
+    }
+}
