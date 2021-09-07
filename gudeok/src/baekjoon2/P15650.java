7ee55654684ddf0d3680512d8007cff7738e36f2
9deok <butterflynine20@gmail.com>
2021-09-08 08:12:48 +0900

    백준 15650 - 3

--- gudeok/src/baekjoon2/P15650.java
+++ gudeok/src/baekjoon2/P15650.java
@@ -1,5 +1,6 @@
 package baekjoon2;
 
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,25 +9,24 @@ import java.util.StringTokenizer;
 public class P15650 {
 
     private static int[] numbers;
+    private static int N, M;
     private static StringBuilder answer = new StringBuilder();
-    private static int N;
-    private static int M;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         numbers = new int[M];
 
-        dfs(1,0);
+        dfs(1, 0);
+
         System.out.println(answer);
     }
 
     private static void dfs(int start, int depth) {
-        if(depth == M) {
-            for(int number : numbers) {
+        if (depth == M) {
+            for (int number : numbers) {
                 answer.append(number).append(" ");
             }
             answer.append("\n");
